3ec68bceadd374346d14c8bde5f5b3cb4ae73944
Samuel Martin <sammartin163598@gmail.com>
2025-07-13 03:07:07 -0500

    Fix formatting by adding a newline at the end of app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from sqlalchemy import create_engine, text
 from werkzeug.security import check_password_hash, generate_password_hash
+
+
