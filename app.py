f7a754c2d84dbe8632a02e820086b19f018ca815
Samuel Martin <sammartin163598@gmail.com>
2025-07-13 02:17:15 -0500

    Add initial app setup and helper functions for error handling and login requirements

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("hello world")
+import os
+
+from datetime import datetime
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from sqlalchemy import create_engine, text
+from werkzeug.security import check_password_hash, generate_password_hash
